1a288bb4acc6b602408b05fa469592f01bae2a96
ankitkrsinha07 <ankitkr.sinha21.07@gmail.com>
2026-09-24 11:13:30 +0530

    Explicitly bind server to 0.0.0.0 for Render port detection

--- src/server.ts
+++ src/server.ts
@@ -35,6 +35,6 @@ app.use((err: any, req: express.Request,
   })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
+app.listen(Number(PORT), '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`)
 })
